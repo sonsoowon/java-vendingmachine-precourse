15f006c561908aaf4f17c94c60adfa3f539779d2
sonsoowon <yuwoo1018@gmail.com>
2022-12-09 06:20:13 +0900

    feat(ProductIndex): 상품 정보 디코딩에 필요한 인덱스 유틸리티 클래스 추가

---
+++ src/main/java/vendingmachine/util/consts/ProductIndex.java
@@ -0,0 +1,10 @@
+package vendingmachine.util.consts;
+
+public class ProductIndex {
+    public static final int NAME = 0;
+    public static final int PRICE = 1;
+    public static final int COUNT = 2;
+
+    private ProductIndex() {
+    }
+}
